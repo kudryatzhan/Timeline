79b8d430028a37d71809d4739d5b9e2a613d55af
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-12-04 15:05:12 -0700

    Model object controller set up

---
+++ Timeline/Model/PostController.swift
@@ -0,0 +1,31 @@
+//
+//  PostController.swift
+//  Timeline
+//
+//  Created by Kudryatzhan Arziyev on 12/4/17.
+//  Copyright © 2017 Kudryatzhan Arziyev. All rights reserved.
+//
+
+import UIKit
+
+class PostController {
+    
+    let sharedController = PostController()
+    
+    var posts = [Post]()
+    
+    // MARK: - Actions
+    func createPostWith(image: UIImage, caption: String) {
+        guard let data = UIImageJPEGRepresentation(image, 0.8) else { return }
+        
+        let post = Post(photoData: data)
+        posts.append(post)
+        
+        let captionComment = Comment(post: post, text: caption)
+    }
+    
+    func addComment(toPost post: Post, textComment: String) {
+        let comment = Comment(post: post, text: textComment)
+        post.comments.append(comment)
+    }
+}
