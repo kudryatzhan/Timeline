2fbc2f4e3d60040f35b682bf113545eac8e55b44
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-12-04 14:30:46 -0700

    Post model created

---
+++ Timeline/Model/Post.swift
@@ -0,0 +1,29 @@
+//
+//  Post.swift
+//  Timeline
+//
+//  Created by Kudryatzhan Arziyev on 12/4/17.
+//  Copyright © 2017 Kudryatzhan Arziyev. All rights reserved.
+//
+
+import UIKit
+
+class Post {
+    
+    // MARK: - Properties
+    let photoData: Data?
+    let timestamp: Date
+    var comments: [Comment]
+    
+    var image: UIImage? {
+        guard let photoData = photoData else { return }
+        return UIImage(data: photoData)
+    }
+    
+    // Initializer
+    init(photoData: Data?, timestamp: Date = Date(), comments: [Comment] = []) {
+        self.photoData = photoData
+        self.timestamp = timestamp
+        self.comments = comments
+    }
+}
