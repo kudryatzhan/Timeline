3003413eaeca3568d3463b93ea408757a3732b67
Kudryatzhan Arziyev <karziyev@gmail.com>
2017-12-04 16:53:36 -0700

    Post Detail Scene added

--- Timeline/View Controllers/PostDetailTableViewController.swift
+++ Timeline/View Controllers/PostDetailTableViewController.swift
@@ -10,88 +10,90 @@ import UIKit
 
 class PostDetailTableViewController: UITableViewController {
     
-    var post: Post?
-
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    var post: Post? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
 
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return post?.comments.count ?? 0
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostDetailTableViewCell", for: indexPath)
+        
+        guard let post = post else { return cell }
+        cell.detailTextLabel?.text = post.comments[indexPath.row].text
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
         if editingStyle == .delete {
-            // Delete the row from the data source
+            post?.comments.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    // MARK: - IBActions
+    @IBAction func commentButtonPressed(_ sender: UIButton) {
+        presentCommentAlert()
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    func presentCommentAlert() {
+        let alertController = UIAlertController(title: "Add Comment", message: nil, preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Add comment here..."
+        }
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            guard let commentText = alertController.textFields?.first?.text,
+                !commentText.isEmpty,
+                let post = self.post else { return }
+            
+            PostController.shared.addComment(toPost: post, textComment: commentText)
+        }
+        alertController.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        
+        
+        present(alertController, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func shareButtonPressed(_ sender: UIButton) {
+        
     }
-    */
-
+    
+    @IBAction func followPostButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    func updateViews() {
+        postImageView.image = post?.image
+        tableView.reloadData()
+    }
 }
